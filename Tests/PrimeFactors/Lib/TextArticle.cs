9bec78f6d3777b94d08997dc941a693cb1a4490a
ericminio <eric.mignot@gmail.com>
2014-06-19 18:54:15 -0400

    ... IntroduceField Decomposition to remove MagicNumber

--- Tests/PrimeFactors/Lib/TextArticle.cs
+++ Tests/PrimeFactors/Lib/TextArticle.cs
@@ -6,12 +6,13 @@ namespace Tests
 	public class TextArticle : Article
 	{
         public int Number;
+        public string Decomposition;
         public string Text;
 
         public void AboutNumber (int number)
         {
             Number = number;
-            Text = Number + " = 2x2x3x5x5 !!!";
+            Text = Number + " = " + Decomposition + " !!!";
         }
 	}
 }

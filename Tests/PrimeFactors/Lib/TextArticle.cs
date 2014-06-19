f339a1d875ba6199d528f1c80cc4d3356a8d4609
ericminio <eric.mignot@gmail.com>
2014-06-19 18:36:59 -0400

    ... Green it with magic number

--- Tests/PrimeFactors/Lib/TextArticle.cs
+++ Tests/PrimeFactors/Lib/TextArticle.cs
@@ -6,6 +6,11 @@ namespace Tests
 	public class TextArticle : Article
 	{
         public string Text;
+
+        public TextArticle()
+        {
+            Text = "300 = 2x2x3x5x5 !!!";
+        }
 	}
 }
 

7821a2cb215336c68d66ad030c2efc8f30f54896
ericminio <eric.mignot@gmail.com>
2014-06-18 21:45:05 -0400

    NUnit can be extended

--- Tests/NUnit.cs
+++ Tests/NUnit.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace Tests
 {
@@ -11,6 +12,45 @@ namespace Tests
         {
             Assert.That (1 + 1, Is.EqualTo (2));
         }
+
+        [Test]
+        public void CanBeExtended()
+        {
+            Assert.That ("Hello World", HasContent.Including ("Hello"));
+        }
     }
+
+    public class HasContent : Constraint
+    {
+        private string Token;
+        private string Actual;
+
+        public HasContent (string token)
+        {
+            this.Token = token;
+        }
+
+        public static HasContent Including (string token)
+        {
+            return new HasContent (token);
+        }
+
+        public override bool Matches (object actual)
+        {
+            this.Actual = (String) actual;
+            return this.Actual.Contains (Token);
+        }
+
+        public override void WriteDescriptionTo (MessageWriter writer)
+        {
+            writer.WriteExpectedValue ("A string containing the folowing token: " + Token);
+        }
+
+        public override void WriteActualValueTo( MessageWriter writer )
+        {
+            writer.WriteActualValue (Actual);
+        }
+    }
+
 }
 
